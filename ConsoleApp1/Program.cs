53c7828291da419b8c1c484176080262aef4dc57
JoelDickel <64815075+JoelDickel@users.noreply.github.com>
2023-01-09 12:36:00 -0300

    First Commit
    Subindo tudo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números?");
+            int a = int.Parse(Console.ReadLine());
+            for(int i = 0; a > i; i++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
